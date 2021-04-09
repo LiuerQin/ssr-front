271eca55822661cc9890df32529aa8d2ef2610e2
260156 <1461065472@qq.com>
2021-04-09 17:29:29 +0800

    fix(uc.vue): api url error

--- pages/uc.vue
+++ pages/uc.vue
@@ -7,7 +7,7 @@
 <script>
 export default {
     async mounted () {
-        const res = await this.$http.get('/user.info')
+        const res = await this.$http.get('/user/info')
         console.log(res)
     }
 }
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,27 +1,49 @@
 import Vue from 'vue'
 import axios from 'axios'
+import { MessageBox } from 'element-ui'
 
 const service = axios.create({
-    // timeout: 5000,
-    baseURL: '/api'
+  // timeout: 5000,
+  baseURL: '/api'
 })
 
-service.interceptors.request.use(
+export default ({
+  store,
+  redirect
+}) => {
+  service.interceptors.request.use(
     async (config) => {
-        const token = localStorage.getItem('token')
-        if (token) {
-            config.headers.common['Authorization'] = `Bearer${token}`
-        }
-        return config
+      const token = localStorage.getItem('token')
+      if (token) {
+        config.headers.common['Authorization'] = `Bearer${token}`
+      }
+      return config
     }
-)
+  )
 
-service.interceptors.response.use(
+  service.interceptors.response.use(
     async (response) => {
-        let {data} = response
-        return data
+      let {
+        data
+      } = response
+      if (data.code === -666) {
+        MessageBox.confirm('登录已过期', '过期', {
+          confirmButtonText: '登录',
+          showCancelButton: false,
+          type: 'warning'
+        }).then(() => {
+          localStorage.removeItem('token')
+          redirect({
+            path: '/login'
+          })
+        })
+      }
+      return data
     }
-)
+  )
+
+}
+
 
 // service.interceptor.request()
 Vue.prototype.$http = service
